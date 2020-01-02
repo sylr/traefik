a1b170e788eccb137254341d1cf03270b83b1133
Sylvain Rabot <s.rabot@lectra.com>
2020-01-02 11:33:41 +0100

    WIP
    Signed-off-by: Sylvain Rabot <s.rabot@lectra.com>

--- pkg/middlewares/accesslog/capture_request_reader.go
+++ pkg/middlewares/accesslog/capture_request_reader.go
@@ -1,18 +1,48 @@
 package accesslog
 
-import "io"
+import (
+	"io"
+	"net/http"
+	"sync"
+)
 
 type captureRequestReader struct {
-	source io.ReadCloser
+	req    *http.Request
 	count  int64
+	closed bool
+	mux    sync.Mutex
 }
 
 func (r *captureRequestReader) Read(p []byte) (int, error) {
-	n, err := r.source.Read(p)
+	if r.closed {
+		return 0, io.EOF
+	}
+
+	r.mux.Lock()
+	defer r.mux.Unlock()
+
+	if r.req != nil && r.req.Body == nil {
+		return 0, nil
+	}
+
+	n, err := r.req.Body.Read(p)
+	if err != nil {
+		return 0, err
+	}
+
 	r.count += int64(n)
 	return n, err
 }
 
 func (r *captureRequestReader) Close() error {
-	return r.source.Close()
+	r.mux.Lock()
+	defer r.mux.Unlock()
+
+	r.closed = true
+
+	if r.req != nil && r.req.Body != nil {
+		return r.req.Body.Close()
+	}
+
+	return io.EOF
 }
