42b1b5394630c541fa554bb4d587f786a1558db4
Sylvain Rabot <sylvain@abstraction.fr>
2022-11-22 11:19:54 +0100

    Add benchmarks for safe wrappers
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- pkg/safe/safe_test.go
+++ pkg/safe/safe_test.go
@@ -1,6 +1,12 @@
 package safe
 
-import "testing"
+import (
+	"runtime"
+	"sync"
+	"testing"
+)
+
+var result int
 
 func TestSafe(t *testing.T) {
 	const ts1 = "test1"
@@ -17,4 +23,66 @@ func TestSafe(t *testing.T) {
 	if result := s.Get(); result != ts2 {
 		t.Errorf("Safe.Get() after Safe.Set() failed, got '%s', expected '%s'", result, ts2)
 	}
+}
+
+func BenchmarkSafeSync(b *testing.B) {
+	s := NewSync[int](1)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
+	// Writing loop
+	go func() {
+		for i := 0; i < b.N; i++ {
+			s.Set(i)
+		}
+
+		wg.Done()
+	}()
+
+	for g := 0; g < runtime.GOMAXPROCS(0); g++ {
+		wg.Add(1)
+
+		go func() {
+			for i := 0; i < b.N; i++ {
+				result = s.Get()
+			}
+
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+}
+
+func BenchmarkSafeAtomic(b *testing.B) {
+	s := NewAtomic[int](1)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
+	// Writing loop
+	go func() {
+		for i := 0; i < b.N; i++ {
+			s.Set(i)
+		}
+
+		wg.Done()
+	}()
+
+	for g := 0; g < runtime.GOMAXPROCS(0); g++ {
+		wg.Add(1)
+
+		go func() {
+			for i := 0; i < b.N; i++ {
+				result = s.Get()
+			}
+
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
 }
